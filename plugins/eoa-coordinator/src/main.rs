903c576712c4351fb3450bcf38bf32336d4dc982
Robert MacWha <github@macwha.com>
2025-12-14 00:26:10 -0500

    feat: coordinator

--- plugins/eoa-coordinator/src/main.rs
+++ plugins/eoa-coordinator/src/main.rs
@@ -8,37 +8,69 @@
 //! is intended purely for demonstration and testing.
 use std::{io::stderr, sync::Arc};
 
-use alloy::{hex, primitives::FixedBytes, signers::local::PrivateKeySigner};
+use alloy::{
+    hex,
+    primitives::{Address, FixedBytes},
+    providers::{Provider, ProviderBuilder},
+    rpc::types::TransactionRequest,
+    signers::local::PrivateKeySigner,
+    sol,
+};
 use serde::{Deserialize, Serialize};
+use tlock_alloy::AlloyBridge;
 use tlock_pdk::{
     server::PluginServer,
     state::{set_state, try_get_state},
     tlock_api::{
         RpcMethod,
         alloy::primitives::U256,
-        caip::{AccountId, AssetId, ChainId},
+        caip::{AccountId, AssetId, AssetType, ChainId},
         component::{button_input, container, form, heading, submit_input, text, text_input},
         coordinator,
         domains::Domain,
-        entities::{CoordinatorId, EntityId, PageId, VaultId},
+        entities::{CoordinatorId, EntityId, EthProviderId, PageId, VaultId},
         global, host,
         page::{self, PageEvent},
         plugin, vault,
     },
-    wasmi_plugin_pdk::{rpc_message::RpcError, transport::JsonRpcTransport},
+    wasmi_plugin_pdk::{
+        rpc_message::{RpcError, to_rpc_err},
+        transport::JsonRpcTransport,
+    },
 };
-use tracing::info;
+use tracing::{info, trace};
 use tracing_subscriber::fmt;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct State {
     vault_id: VaultId,
+    provider_id: EthProviderId,
     coordinator_id: Option<EntityId>,
     private_key: Option<FixedBytes<32>>,
     account: Option<AccountId>,
 }
 
+sol! {
+    #[sol(rpc)]
+    contract ERC20 {
+        function balanceOf(address owner) external view returns (uint256);
+        function transfer(address to, uint256 amount) external returns (bool);
+    }
+}
+
 const CHAIN_ID: u64 = 11155111; // Sepolia
+
+#[derive(Debug)]
+struct ReturnAsset {
+    asset: EvmAsset,
+    deposit_address: Address,
+}
+
+#[derive(Debug)]
+enum EvmAsset {
+    Eth,
+    Erc20(Address),
+}
 
 async fn ping(transport: Arc<JsonRpcTransport>, _: ()) -> Result<String, RpcError> {
     global::Ping.call(transport, ()).await?;
@@ -52,12 +84,18 @@ async fn init(transport: Arc<JsonRpcTransport>, _: ()) -> Result<(), RpcError> {
         .ok_or(RpcError::Custom("Expected vault ID".into()))?;
     info!("Obtained vault ID: {:?}", vault_id);
 
+    let provider_id = host::RequestEthProvider
+        .call(transport.clone(), ChainId::new_evm(CHAIN_ID))
+        .await?
+        .ok_or(RpcError::Custom("Expected provider ID".into()))?;
+
     host::RegisterEntity
         .call(transport.clone(), Domain::Page)
         .await?;
 
     let state = State {
         vault_id,
+        provider_id,
         coordinator_id: None,
         private_key: None,
         account: None,
@@ -132,15 +170,56 @@ async fn propose(
         return Err(RpcError::Custom("Invalid CoordinatorId".into()));
     }
 
-    let Some(state_account_id) = state.account else {
+    let Some(state_account_id) = state.account.clone() else {
         return Err(RpcError::Custom("No Account configured".into()));
+    };
+
+    let Some(state_account_address) = state_account_id.as_evm_address() else {
+        return Err(RpcError::Custom("Account is not an EVM account".into()));
+    };
+
+    let Some(state_private_key) = state.private_key else {
+        return Err(RpcError::Custom("No Private Key configured".into()));
     };
 
     if account_id != state_account_id {
         return Err(RpcError::Custom("Invalid AccountId".into()));
     }
 
-    // Verify that the vault has the necessary assets to cover the bundle.
+    let signer = PrivateKeySigner::from_bytes(&state_private_key).map_err(to_rpc_err)?;
+    let provider = ProviderBuilder::new()
+        .wallet(signer)
+        .connect_client(AlloyBridge::new(
+            transport.clone(),
+            state.provider_id.clone(),
+        ));
+
+    let initial_state_account_balance = provider
+        .get_balance(state_account_address)
+        .await
+        .map_err(to_rpc_err)?;
+
+    verify_vault_balance(&transport, &state, &bundle).await?;
+
+    let return_assets = validate_and_get_return_assets(&transport, &state, &bundle).await?;
+    withdraw_assets(transport, state, &state_account_id, &bundle).await?;
+    execute_bundle(&provider, bundle).await?;
+    return_outstanding_assets(
+        &provider,
+        state_account_address,
+        return_assets,
+        initial_state_account_balance,
+    )
+    .await?;
+
+    Ok(())
+}
+
+async fn verify_vault_balance(
+    transport: &Arc<JsonRpcTransport>,
+    state: &State,
+    bundle: &coordinator::EvmBundle,
+) -> Result<(), RpcError> {
     let vault_assets = vault::GetAssets
         .call(transport.clone(), state.vault_id)
         .await?;
@@ -158,24 +237,213 @@ async fn propose(
         }
     }
 
-    // Transfer requested assets from the vault to the controller's account
+    Ok(())
+}
+
+async fn validate_and_get_return_assets(
+    transport: &Arc<JsonRpcTransport>,
+    state: &State,
+    bundle: &coordinator::EvmBundle,
+) -> Result<Vec<ReturnAsset>, RpcError> {
+    let mut return_assets: Vec<ReturnAsset> = Vec::new();
+
+    let bundled_assets = bundle
+        .inputs
+        .iter()
+        .map(|f| f.0.clone())
+        .chain(bundle.outputs.iter().map(|f| f.clone()));
+
+    for asset_id in bundled_assets {
+        if (asset_id.chain_id) != ChainId::new_evm(CHAIN_ID) {
+            return Err(RpcError::Custom(format!(
+                "Coordinator cannot return asset {:?} on chain {:?}",
+                asset_id, asset_id.chain_id
+            )));
+        }
+
+        let asset = match asset_id.asset {
+            AssetType::Erc20(address) => EvmAsset::Erc20(address),
+            AssetType::Slip44(id) => {
+                if id != 60 {
+                    return Err(RpcError::Custom(format!(
+                        "Coordinator cannot return unsupported slip44 asset {:?}",
+                        asset_id
+                    )));
+                }
+                EvmAsset::Eth
+            }
+            _ => {
+                return Err(RpcError::Custom(format!(
+                    "Coordinator cannot return unsupported asset {:?}",
+                    asset_id
+                )));
+            }
+        };
+
+        let deposit_address = vault::GetDepositAddress
+            .call(transport.clone(), (state.vault_id, asset_id.clone()))
+            .await?;
+
+        let Some(deposit_address) = deposit_address.as_evm_address() else {
+            return Err(RpcError::Custom(format!(
+                "Coordinator cannot return asset {:?} to non-EVM address {:?}",
+                asset_id, deposit_address
+            )));
+        };
+
+        return_assets.push(ReturnAsset {
+            asset,
+            deposit_address,
+        });
+    }
+    Ok(return_assets)
+}
+
+async fn withdraw_assets(
+    transport: Arc<JsonRpcTransport>,
+    state: State,
+    state_account_id: &AccountId,
+    bundle: &coordinator::EvmBundle,
+) -> Result<(), RpcError> {
     for (asset_id, amount) in &bundle.inputs {
         info!("Transferring asset {:?} amount {:?}", asset_id, amount);
+        vault::Withdraw
+            .call(
+                transport.clone(),
+                (
+                    state.vault_id,
+                    state_account_id.clone(),
+                    asset_id.clone(),
+                    amount.clone(),
+                ),
+            )
+            .await?;
     }
 
-    // Perform proposed operations
+    Ok(())
+}
+
+async fn execute_bundle<T: Provider>(
+    provider: &T,
+    bundle: coordinator::EvmBundle,
+) -> Result<(), RpcError> {
     for operation in bundle.operations {
-        info!("Proposed operation: {:?}", operation);
+        info!("Submitting operation: {:?}...", operation);
+        let tx = TransactionRequest::default()
+            .to(operation.to)
+            .input(operation.data.into())
+            .value(operation.value);
+        let tx_hash = provider
+            .send_transaction(tx)
+            .await
+            .map_err(to_rpc_err)?
+            .watch()
+            .await
+            .map_err(to_rpc_err)?;
+        info!("Submitted operation with tx_hash {:?}", tx_hash);
     }
 
-    // Return any assets in the controller's account back to the vault
-    for (asset_id, _) in &bundle.inputs {
-        info!("Returning input asset {:?} to vault", asset_id);
+    Ok(())
+}
+
+async fn return_outstanding_assets<T: Provider>(
+    provider: &T,
+    state_account_address: Address,
+    return_assets: Vec<ReturnAsset>,
+    initial_state_account_balance: U256,
+) -> Result<(), RpcError> {
+    for return_asset in return_assets {
+        info!("Returning {:?} to vault...", &return_asset.asset);
+        match return_asset.asset {
+            EvmAsset::Eth => {
+                return_eth(
+                    provider,
+                    state_account_address,
+                    return_asset.deposit_address,
+                    initial_state_account_balance,
+                )
+                .await?;
+            }
+            EvmAsset::Erc20(address) => {
+                return_erc20(
+                    provider,
+                    state_account_address,
+                    return_asset.deposit_address,
+                    address,
+                )
+                .await?;
+            }
+        }
     }
 
-    for asset_id in &bundle.outputs {
-        info!("Returning output asset {:?} to vault", asset_id);
+    Ok(())
+}
+
+async fn return_eth<T: Provider>(
+    provider: &T,
+    state_account_address: Address,
+    deposit_address: Address,
+    initial_state_account_balance: U256,
+) -> Result<(), RpcError> {
+    let balance = provider
+        .get_balance(state_account_address)
+        .await
+        .map_err(to_rpc_err)?;
+
+    //? Return only the excess balance above the initial balance
+    //? This means that any ETH remaining will first be used to cover gas costs,
+    //? which is generally fine.
+    let return_amount = balance.saturating_sub(initial_state_account_balance);
+    if return_amount == U256::ZERO {
+        trace!("No balance to return, skipping ETH return");
+        return Ok(());
     }
+
+    let tx_hash = provider
+        .send_transaction(
+            TransactionRequest::default()
+                .to(deposit_address)
+                .value(return_amount),
+        )
+        .await
+        .map_err(to_rpc_err)?
+        .watch()
+        .await
+        .map_err(to_rpc_err)?;
+    info!("Returned ETH to vault with tx_hash {:?}", tx_hash);
+    Ok(())
+}
+
+async fn return_erc20<T: Provider>(
+    provider: &T,
+    state_account_address: Address,
+    deposit_address: Address,
+    erc20_address: Address,
+) -> Result<(), RpcError> {
+    let erc20 = ERC20::new(erc20_address, &provider);
+    let balance = erc20
+        .balanceOf(state_account_address)
+        .call()
+        .await
+        .map_err(to_rpc_err)?;
+
+    if balance == U256::ZERO {
+        trace!("No balance for ERC20 {:?}, skipping return", erc20_address);
+        return Ok(());
+    }
+
+    let tx_hash = erc20
+        .transfer(deposit_address, balance)
+        .send()
+        .await
+        .map_err(to_rpc_err)?
+        .watch()
+        .await
+        .map_err(to_rpc_err)?;
+    info!(
+        "Returned ERC20 {:?} to vault with tx_hash {:?}",
+        erc20_address, tx_hash
+    );
 
     Ok(())
 }
